2f3b939aff4f651ddace1ee80be40e742d2ad83e
Murilo Ferraz <murilof.almeida@hotmail.com>
2015-11-11 13:51:49 -0200

    Update SpyInteractive.py

--- SpyInteractive.py
+++ SpyInteractive.py
@@ -14,3 +14,10 @@ capture = cv2.VideoCapture(0)
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),90]
 face_cascade = cv2.CascadeClassifier('/home/root/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('/home/roothaarcascade_eye.xml')
+
+# Listener - Metodo que rece e exibe no serial a mensagem
+def handle(msg):
+	content_type, chat_type, chat_id = telepot.glance2(msg)
+	print (content_type, chat_id)
+	mensagem = msg['text'].strip().lower()
+	print mensagem
