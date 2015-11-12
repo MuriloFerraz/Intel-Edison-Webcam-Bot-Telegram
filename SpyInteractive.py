2c5591fbbb42d2ebf80cb1a54413de13dd63efb2
Murilo Ferraz <murilof.almeida@hotmail.com>
2015-11-12 14:15:57 -0200

    Update SpyInteractive.py

--- SpyInteractive.py
+++ SpyInteractive.py
@@ -7,13 +7,7 @@ import cv2
 import numpy
 import mraa
 
-TOKEN = 'sua chave'
-bot = telepot.Bot(TOKEN)
 
-capture = cv2.VideoCapture(0)
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),90]
-face_cascade = cv2.CascadeClassifier('/home/root/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('/home/roothaarcascade_eye.xml')
 
 # Listener - Metodo que rece e exibe no serial a mensagem
 def handle(msg):
@@ -21,3 +15,25 @@ def handle(msg):
 	print (content_type, chat_id)
 	mensagem = msg['text'].strip().lower()
 	print mensagem
+	
+	
+	if mensagem == 'Hora':
+		bot.sendMessage(chat_id, str(datetime.datetime.now()))
+	
+	else:
+		bot.sendMessage(chat_id, 'Nao consegui entender o comando... \n Tente novamente...')
+
+
+capture = cv2.VideoCapture(0)
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),90]
+face_cascade = cv2.CascadeClassifier('/home/root/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('/home/roothaarcascade_eye.xml')
+
+chat_id = 'chat_id aqui'
+TOKEN = 'sua chave'
+bot = telepot.Bot(TOKEN)
+bot.notifyOnMessage(handle)
+print 'Estou esperando comando ...'
+while 1:
+    time.sleep(10)
+    
